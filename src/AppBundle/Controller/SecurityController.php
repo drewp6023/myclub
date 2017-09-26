4478cf07e645a66a6f745044f0fc9fd59afda491
Drew Pickering <>
2017-09-26 14:41:45 -0600

    Working on login page view and auth

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -3,8 +3,12 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use AppBundle\Entity\Tcontacts;
+use AppBundle\Form\Type\LoginType;
 
 class SecurityController extends Controller
 {
@@ -12,9 +16,39 @@ class SecurityController extends Controller
 
     }
 
-    public function loginAction() {
-        return $this->render('security/login.html.twig', [
+    public function loginAction(Request $request, AuthenticationUtils $authUtils) {
 
+        $contact = new Tcontacts();
+        $form = $this->createForm(LoginType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+
+            // Get all submitted form data
+            $contact = $form->getData();
+
+            $em->persist($contact);
+            $em->flush();
+
+            // $this->addFlash('success', 'You have successfully added contact with ID ' . $contact->getId());
+
+            // Clear the form
+            $this->redirectToRoute('homepage');
+        }
+
+//        // Get the login error if there is one
+//        $error = $authUtils->getLastAuthenticationError();
+//
+//        // last username entered by the user
+//        $lastUsername = $authUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'loginForm' => $form->createView(),
+//            'last_username' => $lastUsername,
+//            'error'         => $error,
         ]);
     }
 }
---
+++ src/AppBundle/Form/Type/LoginType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LoginType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('password', PasswordType::class)
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\Tcontacts'
+        ]);
+    }
+
+}
